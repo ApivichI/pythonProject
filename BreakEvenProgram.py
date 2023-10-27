388b879578e0dbe875e60ce7f406925d03924fcb
ApivichI <66010958@kmitl.ac.th>
2023-10-27 16:08:52 +0700

    add calculate break-even function

--- BreakEvenProgram.py
+++ BreakEvenProgram.py
@@ -1,5 +1,20 @@
 import tkinter as tk
 
+def calculate_break_even():
+    try:
+        fixed_costs = float(fixed_costs_entry.get())
+        variable_costs = float(variable_costs_entry.get())
+        selling_price = float(selling_price_entry.get())
+
+        if variable_costs >= selling_price:
+            result_label.config(text="No Break-Even Point")
+        else:
+            break_even_point = fixed_costs / (selling_price - variable_costs)
+            result_label.config(text=f"Break-Even Point: {break_even_point:.2f} units")
+
+    except ValueError:
+        result_label.config(text="Please enter valid numbers")
+
 app = tk.Tk()
 app.title("Break-Even Analysis Program")
 app.geometry("600x300")
@@ -19,7 +34,10 @@ selling_price_label.pack(anchor = 'center')
 selling_price_entry = tk.Entry()
 selling_price_entry.pack(anchor = 'center')
 
-calculate_button = tk.Button(text="Calculate Break-Even")
+calculate_button = tk.Button(text="Calculate Break-Even", command=calculate_break_even)
 calculate_button.pack(anchor='center')
+
+result_label = tk.Label(text="")
+result_label.pack()
 
 app.mainloop()
