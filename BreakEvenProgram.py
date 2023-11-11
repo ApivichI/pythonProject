6eda7a58fb4f63d969bae122b96d0de6ba64af54
ApivichI <66010958@kmitl.ac.th>
2023-11-11 23:02:36 +0700

    add clear_input function

--- BreakEvenProgram.py
+++ BreakEvenProgram.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
+canvas = None
 
 def calculate_break_even():
     try:
@@ -17,15 +18,21 @@ def calculate_break_even():
 
             plot_break_even(fixed_costs, variable_costs, selling_price, break_even_point)
     except ValueError:
+        clear_input()
         result_label.config(text="Please enter valid numbers")
 
 def plot_break_even(fixed_costs, variable_costs, selling_price, break_even_point):
-    global canvas
-
-    units = list(range(0, int(break_even_point) * 2))
-    revenue = [selling_price * x for x in units]
-    costs = [fixed_costs + variable_costs * x for x in units]
-
+    global canvas          
+    units = []
+    for x in range(0, int(break_even_point) * 2):
+        units.append(x)
+    revenue = []
+    for x in units:
+        revenue.append(selling_price * x)
+    costs = []
+    for x in units:
+        costs.append(fixed_costs + variable_costs * x)
+        
     plt.figure(figsize=(6, 4))
     plt.plot(units, revenue, label="Total Revenue")
     plt.plot(units, costs, label="Total Costs")
@@ -35,13 +42,27 @@ def plot_break_even(fixed_costs, variable_costs, selling_price, break_even_point
     plt.ylabel("Amount")
     plt.title("Break-Even Analysis")
     plt.legend()
+    
+    if canvas is not None:
+        canvas.get_tk_widget().pack_forget() 
 
     app.geometry('800x700')
 
     canvas = FigureCanvasTkAgg(plt.gcf(), master=app)
     canvas.get_tk_widget().pack()
 
-        
+def clear_input():
+    global canvas  
+    result_label.config(text="")
+    fixed_costs_entry.delete(0, "end")
+    variable_costs_entry.delete(0, "end")
+    selling_price_entry.delete(0, "end")
+
+    if canvas is not None:
+        canvas.get_tk_widget().pack_forget()  
+        canvas = None
+        app.geometry('600x300')
+
 app = tk.Tk()
 app.title("Break-Even Analysis Program")
 app.geometry('600x300')
@@ -63,6 +84,9 @@ selling_price_entry.pack(anchor='center')
 
 calculate_button = tk.Button(text="Calculate Break-Even", command=calculate_break_even)
 calculate_button.pack(anchor='center')
+
+cleargraph_button = tk.Button(text="Clear Form And Graph", command=clear_input)
+cleargraph_button.pack(anchor='center')
 
 result_label = tk.Label(text="")
 result_label.pack()
